a12e67387510dfb7a69c21528462c34d922ea14c
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-04-12 17:57:09 -0700

    map proof of concept with markers and popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,42 @@
+import { Loader } from '@googlemaps/js-api-loader';
+import './App.css';
+const loader = new Loader({
+  apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  version: 'weekly',
+  // ...additionalOptions,
+});
+
+loader.load().then(() => {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 45.51223, lng: -122.658722 },
+    zoom: 8,
+  });
+
+  //we will map locations over the marker object
+  const marker1 = new google.maps.Marker({
+    position: { lat: 45.51223, lng: -122.658722 },
+    map: map,
+  });
+  const marker2 = new google.maps.Marker({
+    position: { lat: 45.629398, lng: -122.657107 },
+    map: map,
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: 'here I am',
+  });
+  marker1.addListener('click', () => {
+    infowindow.open({
+      anchor: marker1,
+      map,
+      shouldFocus: false,
+    });
+  });
+});
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <div id="map">map</div>
+    </>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <>
+  <App />
+  </>,
+
+    
 );
