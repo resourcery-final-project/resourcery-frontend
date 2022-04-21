92afe816cca5013698daa1e5197bc65fac160a91
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-04-21 12:05:02 -0700

    stylin profilin

--- src/views/ResourceList/ResourceList.jsx
+++ src/views/ResourceList/ResourceList.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import { getAllResources } from '../../services/resources';
 import List from '../../components/List/List';
+import styles from '../../App.module.css';
+
 
 export default function ResourceList() {
+  const { listview, filter } = styles;
   const [list, setList] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -18,22 +21,23 @@ export default function ResourceList() {
   if (loading) return <h2>Loading list of resources...</h2>;
 
   return (
-    <div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Food Box
-        </label>
-        <label>
-          <input type="checkbox" />
-          Ready to Eat
-        </label>
-        <label>
-          <input type="checkbox" />
-          Fruit Trees
-        </label>
+      <>
+      <div className={filter}>
+      <label>
+        <input type="checkbox" />
+        Food Box
+      </label>
+      <label>
+        <input type="checkbox" />
+        Ready to Eat
+      </label>
+      <label>
+        <input type="checkbox" />
+        Fruit Trees
+      </label>
+    </div><div className={listview}>
+        <List list={list} />
       </div>
-      <List list={list} />
-    </div>
+      </>
   );
 }
