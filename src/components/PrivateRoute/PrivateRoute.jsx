fc50c4bc8d5bbeab77efcb4a7bd9b35dc35b5075
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-04-21 17:35:38 -0700

    put in loading state

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,8 +2,10 @@ import { Redirect, Route, useLocation } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
 
 export default function PrivateRoute({ children, ...routeProps }) {
-  const { user } = useUser();
+  const { user, loading } = useUser();
   const location = useLocation();
+
+  if (loading) return <h2>Loading...</h2>;
 
   return (
     <Route {...routeProps}>
